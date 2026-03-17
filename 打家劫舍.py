5d4c472ba80ae04d5fe1e78c9a3136f840db0f7c
hc932 <108878473@qq.com>
2026-03-17 17:43:05 +0800

    a

---
+++ 打家劫舍.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # @cache
+        # def dfs(i)->int:
+        #     if i<0:
+        #         return 0
+        #     return max(dfs(i-2)+nums[i],dfs(i-1))
+        # return dfs(len(nums)-1)
+        n=len(nums)
+        dp=[0]*(n+2)
+        for i in range(n):
+            dp[i+2]=max(dp[i]+nums[i],dp[i+1])
+        return dp[-1]
+
+
+        
